c287a33dff93ee6905365434ece87347535e6ee6
Roman <github@q-raum.de>
2019-07-10 11:58:14 +0200

    added IP alignment skeleton

---
+++ alignIP/getBoxRotMatrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import uproot
+
+"""
+
+Author: R. Klasen, roklasen@uni-mainz.de or r.klasen@gsi.de
+
+read TrksQA.root files
+find apparent IP position
+compare with actual IP position from PANDA upstream
+create matrix that accounts for box rotation
+save matrix to json file
+rerun Reco and Lumi steps
+"""
+
+
+
+if __name__ == "__main__":
+    pass
