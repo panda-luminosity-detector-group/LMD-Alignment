9c72337ce809712be3571e8bbc8cbac50c8748fc
Roman <github@q-raum.de>
2019-08-09 15:52:53 +0200

    removed unnecccessary stuff

--- detail/LumiValLaTeXTable.py
+++ detail/LumiValLaTeXTable.py
@@ -3,7 +3,7 @@
 """
 Note: of course this will somehow be line based since every line corresponds to a runConfig.
 
-But I want it column based, so that I can just add or remove a column to a Table, 
+But later I want it column based, so that I can just add or remove a column to a Table, 
 set it's header and specify which values from the runConfig go into the column.
 Then, I want to be able to quickly assemble a complete LaTeX table with arbitrary
 columns.
@@ -14,45 +14,19 @@ import json
 class LumiValLaTeXTable:
     def __init__(self):
         self.cols = []
-        print(f'much LaTeX!')
 
     @classmethod
     def fromConfigs(cls, configs):
-        print(f'yay! from configs!')
         temp = cls()
         temp.configs = configs
         return temp
 
-    def addColumn(self, column):
-        self.cols.append(column)
-        pass
-
     def show(self):
         for conf in self.configs:
             print(conf.dump())
-            print(f'nothing more to show!')
 
             with open(conf.pathLumiVals()) as file:
                 lumiVals = json.load(file)
 
             print(f'{conf.momentum} & {conf.misalignType} & {conf.misalignFactor} & {conf.alignmentCorrection} & {lumiVals["relative_deviation_in_percent"]}')
 
-        # TODO: main assembly loop: loop over columns, THEN runConfigs
-        for col in self.cols:
-            print(f'I am a column! Header: {col.header}')
-
-class column:
-    def __init__(self, valueInRunConfig):
-        print(f'empty column')
-        self.header = 'Header!'
-    def val(self):
-        pass
-
-
-class colLumiDev(column):
-    def __init__(self):
-        pass
-    pass
-
-
-# TODO: inherit from column here and implement manually ffs
