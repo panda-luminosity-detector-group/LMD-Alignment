e4d0cc0ebf5b340bc2c3898da39e705b9b31d9b7
Roman Klasen <roklasen@uni-mainz.de>
2019-09-26 10:10:39 +0200

    improved check for env variables

--- detail/LMDRunConfig.py
+++ detail/LMDRunConfig.py
@@ -276,17 +276,28 @@ class LMDRunConfig:
     #! --------------------- upadte env paths, for example when migrating to a different system
     def updateEnvPaths(self):
         alignmentDir = 'LMD_ALIGNMENT_DIR'
-        simDirEnv = 'LMDFIT_DATA_DIR'
         pndRootDir = 'VMCWORKDIR'
+        simDirEnv = 'LMDFIT_DATA_DIR'
         self.__cwd = str(Path.cwd())
+        
         try:
-            self.__simDataPath = os.environ[simDirEnv]
-            self.__pandaRootDir = os.environ[pndRootDir]
             self.__alignmentDir = os.environ[alignmentDir]
         except:
-            print("can't find LuminosityFit installation, PandaRoot installation or Data_Dir!")
-            print(f"please set {pndRootDir} and {simDirEnv}!")
+            print(f"ERROR! Can't find LMDAlignment installation installation, please set {alignmentDir}!")
             sys.exit(1)
+
+        try:
+            self.__pandaRootDir = os.environ[pndRootDir]
+        except:
+            print(f"ERROR! Can't find PandaRoot installation, please set {pndRootDir}!")
+            sys.exit(1)
+        
+        try:
+            self.__simDataPath = os.environ[simDirEnv]
+        except:
+            print(f"ERROR! Can't find, please set {simDirEnv}!")
+            sys.exit(1)
+        
 
     #! --------------------- generate matrix name after minimal initialization
 
