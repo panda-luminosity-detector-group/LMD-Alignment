ef8f6de617221020c2e7e65dee461c24b31500ff
Roman <github@q-raum.de>
2020-02-13 20:07:12 +0100

    updates

--- thesisPlots/plotResidualVsNoOfTracks.py
+++ thesisPlots/plotResidualVsNoOfTracks.py
@@ -8,17 +8,37 @@ from detail.LMDRunConfig import LMDRunConfig
 from alignment.alignerModules import alignerModules
 from detail.matrixComparator import moduleComparator
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
 from pathlib import Path
-import sys, subprocess, json
+import sys, subprocess, json, re
 import numpy as np
 
 np.set_printoptions(precision=3, suppress=True)
 
+latexsigma = r'\textsigma{}'
+latexmu = r'\textmu{}'
+latexPercent = r'\%'
+pipe = r'\textbar{}'
+latexPsi = r'\textPsi{}'
+latexTheta = r'\textTheta{}'
+latexPhi = r'\textPhi{}'
+
+plt.rc('font',**{'family':'serif', 'serif':['Palatino'], 'size':10})
+plt.rc('text', usetex=True)
+plt.rc('text.latex', preamble=r'\usepackage[euler]{textgreek}')
+
+lineOptions = {'capsize':2, 'elinewidth':0.6, 'linewidth':0.4, 'markersize': 10.0}
+
+sizes = [(15/2.54, 6/2.54), (15/2.54, 4/2.54), (6.5/2.54, 4/2.54)]
+offsetscale = 2.5
+offsets = np.arange(-0.02, 0.03, 0.01)
+colors = [u'#1f77b4', u'#ff7f0e', u'#2ca02c', u'#d62728', u'#9467bd', u'#8c564b', u'#e377c2', u'#7f7f7f', u'#bcbd22', u'#17becf']
+
 def dummy():
     # from good-ish tracks
 
-    noOftracks = np.arange(5000,500001, 5000)
+    noOftracks = np.arange(1000,50001, 1000)
 
     resultDict = {}
 
@@ -56,5 +76,88 @@ def dummy():
     with open(f'output/residualVsTrks/nTrksVsResiduals.json', 'w') as f:
         json.dump(resultDict, f, indent=2, sort_keys=True)
 
+def plot():
+
+    if True:
+        jsonFiles = Path('output/residualVsTrks/').glob('[0-9]*.json')
+
+        lines = []
+        for file in jsonFiles:
+            m = re.search(r'ualVsTrks/(\d+)-VsResiduals.js', str(file))
+            if m:
+                nTrk = float(m.group(1))
+            else:
+                continue
+
+            with open(file, 'r') as f:
+                values = np.array(json.load(f)).reshape((40,3))
+                mean, sigma = np.average(values, axis=0), np.std(values, axis=0)
+                line = [float(nTrk), *mean, *sigma]
+                lines.append(line)
+
+    else:
+        with open(f'output/residualVsTrks/nTrksVsResiduals-Hand.json', 'r') as f:
+            data = json.load(f)
+        lines = []
+        for nTrk in data:
+            values = np.array(data[nTrk]).reshape((40, 3))
+            mean, sigma = np.average(values, axis=0), np.std(values, axis=0)
+            line = [float(nTrk), *mean, *sigma]
+            lines.append(line)
+
+    lines = np.array(lines)
+    lines = lines[lines[:,0].argsort()]
+
+    # plot it
+    # title = f'Matrix Residuals {pipe} Standard Deviation'
+    title = f'Module Alignment Matrix Residuals {pipe} Mean'
+    title2 = f'Module Alignment Matrix Residuals {pipe} Standard Deviation'
+    
+    for i in range(len(sizes)):
+
+        fig, ax = plt.subplots(figsize=sizes[i])
+        fig2, ax2 = plt.subplots(figsize=sizes[i])
+        colorI = 0
+
+        # Plotting the error bars
+        ax.errorbar(lines[:,0]-10, lines[:,1], fmt='1', ecolor=colors[colorI], color=colors[colorI], label=f'{latexmu}x', ls='dashed', **lineOptions)
+        ax.errorbar(lines[:,0]+10, lines[:,2], fmt='2', ecolor=colors[colorI+1], color=colors[colorI+1], label=f'{latexmu}y', ls='dashed', **lineOptions)
+
+        ax2.errorbar(lines[:,0]-10, lines[:,4], fmt='1', ecolor=colors[colorI], color=colors[colorI], label=f'{latexsigma}x', ls='dashed', **lineOptions)
+        ax2.errorbar(lines[:,0]+10, lines[:,5], fmt='2', ecolor=colors[colorI+1], color=colors[colorI+1], label=f'{latexsigma}y', ls='dashed', **lineOptions)
+            
+        # Adding plotting parameters
+        ax.set_title(title)
+        ax2.set_title(title2)
+
+        ax.set_xlabel(f'Number of Tracks')
+        ax.set_ylabel(f'Mean [{latexmu}m]')
+        ax2.set_xlabel(f'Number of Tracks')
+        ax2.set_ylabel(f'Standard Deviation [{latexmu}m]')
+        
+        # get handles
+        handles, labels = ax.get_legend_handles_labels()
+        handles = [h[0] for h in handles]
+        ax.legend(handles, labels, loc='center left',numpoints=1)#, bbox_to_anchor=(1, 0.5))
+        ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1f'))
+
+        handles, labels = ax2.get_legend_handles_labels()
+        handles = [h[0] for h in handles]
+        ax2.legend(handles, labels, loc='center left',numpoints=1)#, bbox_to_anchor=(1, 0.5))
+        ax2.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1f'))
+
+        
+        fig.tight_layout()
+        ax.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
+        fig.savefig(f'output/residualVsTrks/noOfTrksVsResidual-mean.pdf', dpi=1000, bbox_inches='tight')
+        plt.close(fig)
+
+        fig2.tight_layout()
+        ax2.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
+        fig2.savefig(f'output/residualVsTrks/noOfTrksVsResidual-std.pdf', dpi=1000, bbox_inches='tight')
+        plt.close(fig2)
+
+        break
+
 if __name__ == "__main__":
-    dummy()
+    plot()
