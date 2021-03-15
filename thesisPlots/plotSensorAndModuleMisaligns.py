03ed1cb7a79f5213c420bbb9f7587f9f03181722
Roman <github@q-raum.de>
2021-03-15 13:40:03 +0100

    matrix compare plotter now also shows rotation

--- thesisPlots/plotSensorAndModuleMisaligns.py
+++ thesisPlots/plotSensorAndModuleMisaligns.py
@@ -27,10 +27,145 @@ plt.rc('text.latex', preamble=r'\usepackage[euler]{textgreek}')
 
 lineOptions = {'capsize': 2, 'elinewidth': 0.6, 'linewidth': 0.4}
 
-sizes = [(17 / 2.54, 7 / 2.54), (15 / 2.54, 4 / 2.54), (6.5 / 2.54, 4 / 2.54)]
+# sizes = [(17 / 2.54, 7 / 2.54), (15 / 2.54, 4 / 2.54), (6.5 / 2.54, 4 / 2.54)]
+sizes = [(17 / 2.54, 7 / 2.54)]  #, (15 / 2.54, 4 / 2.54), (6.5 / 2.54, 4 / 2.54)]
 offsetscale = 1.0
 offsets = np.arange(-0.02, 0.03, 0.01)
 colors = [u'#1f77b4', u'#ff7f0e', u'#2ca02c', u'#d62728', u'#9467bd', u'#8c564b', u'#e377c2', u'#7f7f7f', u'#bcbd22', u'#17becf']
+
+
+def omniPlots(sensorVals, i, momenta, outTypeName):
+
+    fig, ax = plt.subplots(figsize=sizes[i])
+    fig2, ax2 = plt.subplots(figsize=sizes[i])
+    fig3, ax3 = plt.subplots(figsize=sizes[i])
+    fig4, ax4 = plt.subplots(figsize=sizes[i])
+    colorI = 0
+
+    for mom in momenta:
+
+        mask = (sensorVals[:, 0] == float(mom))
+        thseVals = sensorVals[mask]
+
+        print(f'Ayy lmao I will print this (Sensor Plot):\n{thseVals}')
+
+        # sort 2D array by second column
+        #* order in list: [mom, fac, dX, dY, dR, stdX, stdY, stdR], units are microns and mrad
+        thseVals = thseVals[thseVals[:, 1].argsort()]
+
+        # Plot dx, dx MEAN
+        ax.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
+                    thseVals[:, 2],
+                    fmt='2',
+                    ecolor=colors[colorI],
+                    color=colors[colorI],
+                    label=f'x @ {mom} GeV/c',
+                    ls='dashed',
+                    **lineOptions)
+        ax.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale + 0.00,
+                    thseVals[:, 3],
+                    fmt='1',
+                    ecolor=colors[colorI],
+                    color=colors[colorI],
+                    label=f'y @ {mom} GeV/c',
+                    ls='dashed',
+                    **lineOptions)
+
+        # Plot dx, dx STD
+        ax2.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
+                     thseVals[:, 5],
+                     fmt='2',
+                     ecolor=colors[colorI],
+                     color=colors[colorI],
+                     label=f'x @ {mom} GeV/c',
+                     ls='dashed',
+                     **lineOptions)
+        ax2.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale + 0.00,
+                     thseVals[:, 6],
+                     fmt='1',
+                     ecolor=colors[colorI],
+                     color=colors[colorI],
+                     label=f'y @ {mom} GeV/c',
+                     ls='dashed',
+                     **lineOptions)
+
+        # Plot dR MEAN
+        ax3.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
+                     thseVals[:, 4] ,
+                     fmt='2',
+                     ecolor=colors[colorI],
+                     color=colors[colorI],
+                     label=f'x @ {mom} GeV/c',
+                     ls='dashed',
+                     **lineOptions)
+
+        # Plot dR STD
+        ax4.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
+                     thseVals[:, 7] ,
+                     fmt='2',
+                     ecolor=colors[colorI],
+                     color=colors[colorI],
+                     label=f'x @ {mom} GeV/c',
+                     ls='dashed',
+                     **lineOptions)
+
+        colorI += 1
+
+    # Adding plotting parameters
+
+    ax.set_xlabel(f'Misalign Factor')
+    ax2.set_xlabel(f'Misalign Factor')
+    ax3.set_xlabel(f'Misalign Factor')
+    ax4.set_xlabel(f'Misalign Factor')
+    ax.set_ylabel(f'Mean [{latexmu}m]')
+    ax2.set_ylabel(f'Standard Deviation [{latexmu}m]')
+    ax3.set_ylabel(f'Mean [mrad]')
+    ax4.set_ylabel(f'Standard Deviation [mrad]')
+
+    ax.xaxis.set_ticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0])
+    ax2.xaxis.set_ticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0])
+    ax3.xaxis.set_ticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0])
+    ax4.xaxis.set_ticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0])
+
+    start, end = ax.get_xlim()
+    ax.xaxis.set_view_interval(start, 3.172)
+    ax2.xaxis.set_view_interval(start, 3.172)
+    ax3.xaxis.set_view_interval(start, 3.172)
+    ax4.xaxis.set_view_interval(start, 3.172)
+
+    # get handles
+    handles, labels = ax.get_legend_handles_labels()
+    handles = [h[0] for h in handles]
+    ax.legend(handles, labels, loc='center left', numpoints=1, bbox_to_anchor=(1, 0.5))
+    handles, labels = ax2.get_legend_handles_labels()
+    handles = [h[0] for h in handles]
+    ax2.legend(handles, labels, loc='center left', numpoints=1, bbox_to_anchor=(1, 0.5))
+    handles, labels = ax3.get_legend_handles_labels()
+    handles = [h[0] for h in handles]
+    ax3.legend(handles, labels, loc='center left', numpoints=1, bbox_to_anchor=(1, 0.5))
+    handles, labels = ax4.get_legend_handles_labels()
+    handles = [h[0] for h in handles]
+    ax4.legend(handles, labels, loc='center left', numpoints=1, bbox_to_anchor=(1, 0.5))
+
+    fig.tight_layout()
+    ax.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
+    fig.savefig(f'mis-{sys.argv[2]}-residual-{outTypeName}-{i}-mean.pdf', dpi=1000, bbox_inches='tight')
+    plt.close(fig)
+
+    fig2.tight_layout()
+    ax2.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
+    fig2.savefig(f'mis-{sys.argv[2]}-residual-{outTypeName}-{i}-std.pdf', dpi=1000, bbox_inches='tight')
+    plt.close(fig2)
+
+    fig3.tight_layout()
+    ax3.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
+    fig3.savefig(f'mis-{sys.argv[2]}-residual-{outTypeName}-{i}-rot-mean.pdf', dpi=1000, bbox_inches='tight')
+    plt.close(fig2)
+
+    fig4.tight_layout()
+    ax4.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
+    fig4.savefig(f'mis-{sys.argv[2]}-residual-{outTypeName}-{i}-rot-std.pdf', dpi=1000, bbox_inches='tight')
+    plt.close(fig2)
 
 
 def plotSensorMatrices(inputJson, outFileName):
@@ -50,93 +185,8 @@ def plotSensorMatrices(inputJson, outFileName):
     momenta = np.sort(momenta)
     momenta = np.unique(momenta, axis=0)
 
-    # title = f'Matrix Residuals {pipe} Standard Deviation'
-    title = f'Sensor Alignment Matrix Residuals {pipe} Mean'
-    title2 = f'Sensor Alignment Matrix Residuals {pipe} Standard Deviation'
-
     for i in range(len(sizes)):
-
-        fig, ax = plt.subplots(figsize=sizes[i])
-        fig2, ax2 = plt.subplots(figsize=sizes[i])
-        colorI = 0
-
-        for mom in momenta:
-
-            mask = (sensorVals[:, 0] == float(mom))
-            thseVals = sensorVals[mask]
-
-            # sort 2D array by second column
-            thseVals = thseVals[thseVals[:, 1].argsort()]
-
-            # Plotting the error bars
-            ax.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
-                        thseVals[:, 2],
-                        fmt='2',
-                        ecolor=colors[colorI],
-                        color=colors[colorI],
-                        label=f'x @ {mom} GeV/c',
-                        ls='dashed',
-                        **lineOptions)
-            ax.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale + 0.00,
-                        thseVals[:, 3],
-                        fmt='1',
-                        ecolor=colors[colorI],
-                        color=colors[colorI],
-                        label=f'y @ {mom} GeV/c',
-                        ls='dashed',
-                        **lineOptions)
-            ax2.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
-                         thseVals[:, 5],
-                         fmt='2',
-                         ecolor=colors[colorI],
-                         color=colors[colorI],
-                         label=f'x @ {mom} GeV/c',
-                         ls='dashed',
-                         **lineOptions)
-            ax2.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale + 0.00,
-                         thseVals[:, 6],
-                         fmt='1',
-                         ecolor=colors[colorI],
-                         color=colors[colorI],
-                         label=f'y @ {mom} GeV/c',
-                         ls='dashed',
-                         **lineOptions)
-
-            colorI += 1
-
-        # Adding plotting parameters
-
-        ax.set_xlabel(f'Misalign Factor')
-        ax2.set_xlabel(f'Misalign Factor')
-        ax.set_ylabel(f'Mean [{latexmu}m]')
-        ax2.set_ylabel(f'Standard Deviation [{latexmu}m]')
-
-        ax.xaxis.set_ticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0])
-        ax2.xaxis.set_ticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0])
-
-        start, end = ax.get_xlim()
-        ax.xaxis.set_view_interval(start, 3.172)
-        ax2.xaxis.set_view_interval(start, 3.172)
-
-        # get handles
-        handles, labels = ax.get_legend_handles_labels()
-        handles = [h[0] for h in handles]
-        ax.legend(handles, labels, loc='center left', numpoints=1, bbox_to_anchor=(1, 0.5))
-        handles, labels = ax2.get_legend_handles_labels()
-        handles = [h[0] for h in handles]
-        ax2.legend(handles, labels, loc='center left', numpoints=1, bbox_to_anchor=(1, 0.5))
-
-        fig.tight_layout()
-        ax.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
-        fig.savefig(f'mis-{sys.argv[2]}-residual-sensors-{i}-mean.pdf', dpi=1000, bbox_inches='tight')
-        plt.close(fig)
-
-        fig2.tight_layout()
-        ax2.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
-        fig2.savefig(f'mis-{sys.argv[2]}-residual-sensors-{i}-std.pdf', dpi=1000, bbox_inches='tight')
-        plt.close(fig2)
-
-        break
+        omniPlots(sensorVals, i, momenta, 'sensors')
 
 
 def plotModuleMatrices(inputJson, outFileName):
@@ -162,87 +212,7 @@ def plotModuleMatrices(inputJson, outFileName):
 
     for i in range(len(sizes)):
 
-        fig, ax = plt.subplots(figsize=sizes[i])
-        fig2, ax2 = plt.subplots(figsize=sizes[i])
-        colorI = 0
-
-        for mom in momenta:
-
-            mask = (moduleVals[:, 0] == float(mom))
-            thseVals = moduleVals[mask]
-
-            # sort 2D array by second column
-            thseVals = thseVals[thseVals[:, 1].argsort()]
-
-            # Plotting the error bars
-            ax.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
-                        thseVals[:, 2],
-                        fmt='2',
-                        ecolor=colors[colorI],
-                        color=colors[colorI],
-                        label=f'x @ {mom} GeV/c',
-                        ls='dashed',
-                        **lineOptions)
-            ax.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale + 0.00,
-                        thseVals[:, 3],
-                        fmt='1',
-                        ecolor=colors[colorI],
-                        color=colors[colorI],
-                        label=f'y @ {mom} GeV/c',
-                        ls='dashed',
-                        **lineOptions)
-            ax2.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
-                         thseVals[:, 5],
-                         fmt='2',
-                         ecolor=colors[colorI],
-                         color=colors[colorI],
-                         label=f'x @ {mom} GeV/c',
-                         ls='dashed',
-                         **lineOptions)
-            ax2.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale + 0.00,
-                         thseVals[:, 6],
-                         fmt='1',
-                         ecolor=colors[colorI],
-                         color=colors[colorI],
-                         label=f'y @ {mom} GeV/c',
-                         ls='dashed',
-                         **lineOptions)
-
-            colorI += 1
-
-        # Adding plotting parameters
-
-        ax.set_xlabel(f'Misalign Factor')
-        ax2.set_xlabel(f'Misalign Factor')
-        ax.set_ylabel(f'Mean [{latexmu}m]')
-        ax2.set_ylabel(f'Standard Deviation [{latexmu}m]')
-
-        ax.xaxis.set_ticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0])
-        ax2.xaxis.set_ticks([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0])
-
-        start, end = ax.get_xlim()
-        ax.xaxis.set_view_interval(start, 3.172)
-        ax2.xaxis.set_view_interval(start, 3.172)
-
-        # get handles
-        handles, labels = ax.get_legend_handles_labels()
-        handles = [h[0] for h in handles]
-        ax.legend(handles, labels, loc='center left', numpoints=1, bbox_to_anchor=(1, 0.5))
-        handles, labels = ax2.get_legend_handles_labels()
-        handles = [h[0] for h in handles]
-        ax2.legend(handles, labels, loc='center left', numpoints=1, bbox_to_anchor=(1, 0.5))
-
-        fig.tight_layout()
-        ax.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
-        fig.savefig(f'mis-{sys.argv[2]}-residual-modules-{i}-mean.pdf', dpi=1000, bbox_inches='tight')
-        plt.close(fig)
-
-        fig2.tight_layout()
-        ax2.grid(color='lightgrey', which='major', axis='both', linestyle='dotted')
-        fig2.savefig(f'mis-{sys.argv[2]}-residual-modules-{i}-std.pdf', dpi=1000, bbox_inches='tight')
-        plt.close(fig2)
-
-        break
+        omniPlots(moduleVals, i, momenta, 'modules')
 
 
 def plotBoxMatrices(inputJson, outFileName):
@@ -279,16 +249,16 @@ def plotBoxMatrices(inputJson, outFileName):
             # sort 2D array by second column
             thseVals = thseVals[thseVals[:, 1].argsort()]
 
-            # The box rot values are ABSOLUTE, not residual. That means we have to subtract the design value by hand to get the residual
+            #! The box rot values are ABSOLUTE, not residual. That means we have to subtract the design value by hand to get the residual
             # take care! I don't know of all data is generated the same way, you have to check each set individually :(
-            if False:
+            if True:
                 fixScale = 100  # the amount of misalignment at factor 1.0
                 fixFactors = np.array([0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0]) * fixScale
-                print(f'Fix Table:\n{fixFactors}')
-                thseVals[:,2] += fixFactors
-                thseVals[:,3] -= fixFactors
+                print(f'Fix Table (Box Plot):\n{fixFactors}')
+                thseVals[:, 2] += fixFactors
+                thseVals[:, 3] -= fixFactors
 
-            print(f'Ayy lmao I will print this:\n{thseVals}')
+            print(f'Ayy lmao I will print this (Box Plot):\n{thseVals}')
 
             # Plotting the error bars                                                   , yerr=thseVals[:,3]
             ax.errorbar(thseVals[:, 1] + offsets[colorI] * offsetscale - 0.00,
@@ -345,6 +315,7 @@ if __name__ == "__main__":
     # read configs?
     if len(sys.argv) != 3:
         print(f'please specify allMatrixValues.json and outputName (sensors, modules or box100)!')
+        print('eg: thesisPlots/plotSensorAndModuleMisaligns.py runConfigs/corrected/box100/allMatrixValues.json box100')
         sys.exit(1)
 
     with open(sys.argv[1], 'r') as f:
