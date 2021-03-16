c9f1a0010dbc76f46434906267e728c36e1786fa
Roman <github@q-raum.de>
2021-03-16 22:19:56 +0100

    track residual plot now also does 1D

--- thesisPlots/histTrackResiduals.py
+++ thesisPlots/histTrackResiduals.py
@@ -3,6 +3,7 @@
 # from alignment.modules.trackReader import trackReader
 from pathlib import Path
 from matplotlib.ticker import NullFormatter
+from matplotlib.ticker import MaxNLocator
 
 import numpy as np
 import matplotlib
@@ -53,37 +54,48 @@ def doit():
 
         for mom in ['1.5', '15.0']:
 
-            # fileName = f'input/trackResiduals/processedTracks-{mom}-GeV-aligned.json'
-            fileName = f'input/trackResiduals/processedTracks-{mom}-GeV-aligned.json'
+            npName = f'input/trackResiduals/procTrks-{mom}.npy'
 
-            # load track file
-            reader = foo.trackReader()
-            reader.readDetectorParameters()
-            reader.readTracksFromJson(fileName)
-            allTracks = reader.getAllTracksInSector(sector)
+            if Path(npName).exists():
+                print(f'np Tracks found, loading...')
+                newTracks = np.load(npName)
+                nTrks = len(newTracks)
+            
+            else:
 
-            print(f'tracks read from json, cleaning up...')
+                fileName = f'input/trackResiduals/processedTracks-{mom}-GeV-aligned.json'
 
-            #? new format! np array with track oris, track dirs, and recos
-            nTrks = len(allTracks)
-            newTracks = np.ones((nTrks, 6, 4))
+                # load track file
+                reader = foo.trackReader()
+                reader.readDetectorParameters()
+                reader.readTracksFromJson(fileName)
+                allTracks = reader.getAllTracksInSector(sector)
 
-            for i in range(nTrks):
-                newTracks[i, 0, :3] = allTracks[i]['trkPos']
-                newTracks[i, 1, :3] = allTracks[i]['trkMom']
-                newTracks[i, 2, :3] = allTracks[i]['recoHits'][0]['pos']
-                newTracks[i, 3, :3] = allTracks[i]['recoHits'][1]['pos']
-                newTracks[i, 4, :3] = allTracks[i]['recoHits'][2]['pos']
-                newTracks[i, 5, :3] = allTracks[i]['recoHits'][3]['pos']
+                print(f'tracks read from json, cleaning up...')
 
-            newTracks = newTracks[:900000]
-            nTrks = len(newTracks)
-            print(f'I will now plot {len(newTracks)} tracks.')
+                #? new format! np array with track oris, track dirs, and recos
+                nTrks = len(allTracks)
+                newTracks = np.ones((nTrks, 6, 4))
 
+                for i in range(nTrks):
+                    newTracks[i, 0, :3] = allTracks[i]['trkPos']
+                    newTracks[i, 1, :3] = allTracks[i]['trkMom']
+                    newTracks[i, 2, :3] = allTracks[i]['recoHits'][0]['pos']
+                    newTracks[i, 3, :3] = allTracks[i]['recoHits'][1]['pos']
+                    newTracks[i, 4, :3] = allTracks[i]['recoHits'][2]['pos']
+                    newTracks[i, 5, :3] = allTracks[i]['recoHits'][3]['pos']
+
+                # newTracks = newTracks[:900000]
+                nTrks = len(newTracks)
+
+                print(f'saving np Tracks...')
+                np.save(npName, newTracks, False)
+                
             if useCut and True:
                 newTracks = dynamicRecoTrackDistanceCut(newTracks, 0.5)
                 nTrks = len(newTracks)
 
+            print(f'I will now plot {len(newTracks)} tracks.')
             distMultiArray = np.zeros((4, nTrks, 3))
 
             print(f'done, creating histograms...')
@@ -102,7 +114,7 @@ def doit():
 
                 distMultiArray[i] = dVec
 
-            #* ----------------- begin hist here
+            #* ----------------- hist preparation
             import matplotlib
             import matplotlib.pyplot as plt
             from matplotlib.colors import LogNorm
@@ -127,12 +139,44 @@ def doit():
             if useCut:
                 myLimit /= 10
             myRange = ((-myLimit, myLimit), (-myLimit, myLimit))
-            bins = 150
+            bins = 100
+            dunit = f'd [{latexmu}m]'
             xunit = f'dx [{latexmu}m]'
             yunit = f'dy [{latexmu}m]'
 
+            #* -------------- begin 1d hist here
+
             for i in range(4):
-                sigma = np.round(np.std(distMultiArray[i, :, 0] * scaleFact), 2)
+                # calculate 1D distance
+                # distances1D = np.linalg.norm(distMultiArray[i], axis=1)
+
+                sigmax = np.std(distMultiArray[i, :, 0] * scaleFact)
+                axs[i].hist(distMultiArray[i,:,0] * scaleFact, bins=bins, log=True, range=(-myLimit,myLimit), rasterized=True)
+                axs[i].set_title(f'Plane {i+1}')
+                axs[i].set_xlabel(f'{xunit}')
+                axs[i].set_xlabel(f'{xunit}\n{latexsigma}x={sigmax:.0f}{latexmu}m')
+                axs[i].set_ylim((1e1,10000000))
+                axs[i].yaxis.set_ticks([1e2, 1e3, 1e4, 1e5, 1e6])  
+                axs[i].grid(color='grey', which='major', axis='y', linestyle='dotted')
+
+            axs[0].set_ylabel('Entries (log)')
+
+            fig.set_size_inches((15 / 2.54, 4 / 2.54))
+            fig.subplots_adjust(wspace=0, hspace=0)
+            if useCut:
+                fig.savefig(f'withCut-{mom}-1D.pdf', dpi=300, bbox_inches='tight', pad_inches=0.05)
+            else:
+                fig.savefig(f'noCut-{mom}-1D.pdf', dpi=300, bbox_inches='tight', pad_inches=0.05)
+            
+            
+            plt.close(fig)
+            fig, axs = plt.subplots(1, 4, sharex=True, sharey=True)
+
+            #* -------------- begin 2d hist here
+
+            for i in range(4):
+                sigmax = np.std(distMultiArray[i, :, 0] * scaleFact)
+                sigmay = np.std(distMultiArray[i, :, 1] * scaleFact)
                 axs[i].hist2d(distMultiArray[i, :, 0] * scaleFact,
                               distMultiArray[i, :, 1] * scaleFact,
                               bins=bins,
@@ -142,17 +186,16 @@ def doit():
                               rasterized=True)
                 axs[i].set_title(f'Plane {i+1}')
                 axs[i].set_aspect('equal', 'box')
-                axs[i].set_xlabel(f'{xunit}\n{latexsigma}={sigma}{latexmu}m')
+                axs[i].set_xlabel(f'{xunit}\n{latexsigma}={sigmax:.0f}{latexmu}m')
 
             axs[0].set_ylabel(yunit)
 
-            fig.set_size_inches((14 / 2.54, 5 / 2.54))
+            fig.set_size_inches((15 / 2.54, 5 / 2.54))
             fig.subplots_adjust(wspace=0, hspace=0)
-            fig.tight_layout()
             if useCut:
-                fig.savefig(f'withCut-{mom}.pdf')
+                fig.savefig(f'withCut-{mom}.pdf', dpi=300, bbox_inches='tight', pad_inches=0.05)
             else:
-                fig.savefig(f'noCut-{mom}.pdf')
+                fig.savefig(f'noCut-{mom}.pdf', dpi=300, bbox_inches='tight', pad_inches=0.05)
             plt.close(fig)
 
 
